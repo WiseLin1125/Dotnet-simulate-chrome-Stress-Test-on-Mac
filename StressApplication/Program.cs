68f18d96969c4c9bf7cc7589cbdca9f3a3e49611
WiseLin <wiselin1125@gmail.com>
2017-10-09 22:36:22 +0800

    Optimize the page stree tool
    1. Updating the chromedrive's path to use global paramenter "path"
    2. Extracting the attack behavior as a function
    3. Adding an interface "IExecuteSeleniumLoadPages" for loading the pages via each broswer such as Chrome, Safari and so on.
    4. Revising some int parameters to inline method.

--- StressApplication/Program.cs
+++ StressApplication/Program.cs
@@ -3,30 +3,27 @@
 namespace Stress_Console {
 	class MainClass
 	{
-		const int timeoutSec = 20;
+		public const int TimeoutSec = 20;
 		//const string seanUrl = "https://checed.botsnova.com/";
 		//const string nissanUrl = "http://new.nissan.com.tw/nissan/";
-		static readonly string path = Directory.GetCurrentDirectory();
+        public static readonly string Path = Directory.GetCurrentDirectory();
         static IConfigurationRoot Configuration; 
-		public static void Main(string[] args)
+		public static void Main()
 		{
 			try
             {
-				var builder = new ConfigurationBuilder()
-        		.SetBasePath(Directory.GetCurrentDirectory())
-        		.AddJsonFile("appsettings.json");
-				Configuration = builder.Build(); 
-                int attackTimes;
-                Console.Write("How many 'Times' do you want to attack?(Default: {0} times) ",Configuration["defaultAttackTimes"]);
+				var builder = new ConfigurationBuilder()                     .SetBasePath(Path)                     .AddJsonFile("appsettings.json");
+                Configuration = builder.Build();
+
+                Console.Write("How many 'Times' do you want to attack?(Default: {0} times) ", Configuration["defaultAttackTimes"]);
                 var attackTimesTmp = Console.ReadLine();
-				if (!int.TryParse(attackTimesTmp, out attackTimes) || string.IsNullOrEmpty(attackTimesTmp))
+				if (!int.TryParse(attackTimesTmp, out int attackTimes) || string.IsNullOrEmpty(attackTimesTmp))
 					attackTimes = int.Parse(Configuration["defaultAttackTimes"]);
 				Console.WriteLine(attackTimes + " times\n");
 
-				int threadCount;
-                Console.Write("How many 'Threads' do you want to create?(Default: {0} threads) ",Configuration["defaultThreadCount"]);
-				var threadCountTmp = Console.ReadLine();
-				if (!int.TryParse(threadCountTmp, out threadCount) || string.IsNullOrEmpty(threadCountTmp))
+                Console.Write("How many 'Threads' do you want to create?(Default: {0} threads) ", Configuration["defaultThreadCount"]);
+                var threadCountTmp = Console.ReadLine();
+				if (!int.TryParse(threadCountTmp, out int threadCount) || string.IsNullOrEmpty(threadCountTmp))
 					threadCount = int.Parse(Configuration["defaultThreadCount"]);
                 Console.WriteLine(threadCount + " threads\n");
 
@@ -36,10 +33,9 @@ namespace Stress_Console {
 				string exeUrl = string.IsNullOrEmpty(exeUrlTmp) ? Configuration["defaultExecutionUrl"] : exeUrlTmp;
 				Console.WriteLine("We're going to attack " + exeUrl + "!!\n");
 
-				int suspendSeconds;
-                Console.Write("How many seconds do you want to suspend in each attack?(Default: {0} second) ",Configuration["defaultSuspendSeconds"]);
-				string suspendSecondsTmp = Console.ReadLine();
-				if (!int.TryParse(suspendSecondsTmp, out suspendSeconds) || string.IsNullOrEmpty(suspendSecondsTmp))
+                Console.Write("How many seconds do you want to suspend in each attack?(Default: {0} second) ", Configuration["defaultSuspendSeconds"]);
+                string suspendSecondsTmp = Console.ReadLine();
+				if (!int.TryParse(suspendSecondsTmp, out int suspendSeconds) || string.IsNullOrEmpty(suspendSecondsTmp))
 					suspendSeconds = int.Parse(Configuration["defaultSuspendSeconds"]);
 				Console.WriteLine(suspendSeconds + " seconds\n");
 
@@ -47,33 +43,24 @@ namespace Stress_Console {
 				sw.Reset();
 				sw.Start();
                  Console.WriteLine("Start at: {0}", DateTime.Now.ToString());
-				Thread[] workers = new Thread[threadCount];
-				int jobsCountPerWorker = attackTimes / threadCount;                  //using (IWebDriver driver = new ChromeDriver(path))                 //            {                 //    string existingWindowHandle = driver.CurrentWindowHandle;                 //    var driver2 = driver;                 //                driver.Url = exeUrl;                 //                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSec));                 //                IWebElement element = wait.Until(x => x.FindElement(By.ClassName("toolbar")));                 //                IWebElement myDynamicElement = wait.Until<IWebElement>(d => d.FindElement(By.Id("UradTracking")));                 //                //var test = driver.FindElement(By.Id("fb-root"));                 //                Console.WriteLine("attacked!");                 //}             
+				Thread[] threads = new Thread[threadCount];
+                int jobsCountPerThread = attackTimes % threadCount==0?attackTimes / threadCount:(attackTimes / threadCount)+1;                  IExecuteSeleniumLoadPages executeSelenium; 
 				for (int i = 0; i < threadCount; i++)
-				{
-					int st = jobsCountPerWorker * i;
-					int ed = jobsCountPerWorker * (i + 1);
+                {
+					int st = jobsCountPerThread * i;
+					int ed = jobsCountPerThread * (i + 1);
 					if (ed > attackTimes) ed = attackTimes;
-					workers[i] = new Thread(() =>
+					threads[i] = new Thread(() =>
 					{
                         for (int j = st; j < ed; j++)
-                        {
-                            /// Start to attack
-                            using (IWebDriver driver = new ChromeDriver(path))
-                            {                                 driver.Url = exeUrl;
-                                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutSec));
-                                //IWebElement element = wait.Until(x => x.FindElement(By.ClassName("toolbar")));
-                                //IWebElement myDynamicElement = wait.Until<IWebElement>(d => d.FindElement(By.Id("UradTracking")));
-                                Console.WriteLine("No." + j + " attacked!");
-                                Thread.Sleep(suspendSeconds * 1000);
-                            }
+                        {                             executeSelenium = new SeleniumChrome();
+                            executeSelenium.LoadPages(exeUrl,suspendSeconds,j);
                         }
-
 					});
-					workers[i].Start();
+					threads[i].Start();
 				}
-				for (int i = 0; i < threadCount; i++)
-					workers[i].Join();
+				//for (int i = 0; i < threadCount; i++)
+					//workers[i].Join();
 				sw.Stop();                 Console.WriteLine("End at: {0}", DateTime.Now.ToString());
 				Console.WriteLine("\nAttacked completely! Congratulations!!");
 				decimal spentSeconds = decimal.Parse(sw.ElapsedMilliseconds.ToString());
@@ -85,4 +72,4 @@ namespace Stress_Console {
 				Console.Read();
 			}
 		}
-	} }  
+	}       /// <summary>     /// Execute selenium load pages interface.     /// </summary>     interface IExecuteSeleniumLoadPages     {         void LoadPages(string exeUrl, int suspendSeconds, int attackSerialNum);     }      /// <summary>     /// Selenium chrome.     /// </summary>     class SeleniumChrome: IExecuteSeleniumLoadPages     {         public void LoadPages(string exeUrl, int suspendSeconds, int attackSerialNum)         {             using (IWebDriver driver = new ChromeDriver(MainClass.Path))             {                 driver.Url = exeUrl;                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(MainClass.TimeoutSec));                 //IWebElement element = wait.Until(x => x.FindElement(By.ClassName("toolbar")));                 //IWebElement myDynamicElement = wait.Until<IWebElement>(d => d.FindElement(By.Id("UradTracking")));                 Console.WriteLine("No." + attackSerialNum + " attacked!");                 Thread.Sleep(suspendSeconds * 1000);             }         }     }      /// <summary>     /// Selenium Safari.     /// </summary>     class SeleniumSafari: IExecuteSeleniumLoadPages     {         public void LoadPages(string exeUrl, int suspendSeconds, int attackSerialNum)         {                      }     } }  
